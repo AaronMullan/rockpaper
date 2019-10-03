01953e9aea45e030354535418e22e08c69cdb932
AaronMullan <aaroncmullan@gmail.com>
2019-10-02 20:50:51 -0700

    working on tests

---
+++ app.js
@@ -0,0 +1,11 @@
+// initialize/fetch DOM elements
+
+// set some initial state
+
+// define DOM utility function
+
+
+
+
+// add event listeners
+
--- test/rockpapertest.js
+++ test/rockpapertest.js
@@ -1,16 +1,17 @@
 // IMPORT MODULES under test here:
-// import example from '../src/example.js';  
+import checkResult from '../battleresult.js';  
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('if user selection defeats Godzilla, return 1', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    const userAttack = 'rock';
+    const godzillaAttack = 'scissors';
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const result = checkResult(userAttack, godzillaAttack);
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(result, 1);
 });
