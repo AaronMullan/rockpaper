ec5b372a0550f836c2c9cd7340fd51f7f2d108d3
AaronMullan <aaroncmullan@gmail.com>
2019-10-03 05:48:55 -0700

    added some variables

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 // initialize/fetch DOM elements
 const attackButton = document.getElementById('attack');
+let randomGodzillaattack;
+let godzillaLosesWithRock;
+let gozillaLosesWithPaper;
+let godzillaLosesWithScissors;
+
 
 // set some initial state
-let userAttack = ' ';
+let userAttack = 'rock';
+let godzillaAttack = 'rock';
+
 // define DOM utility function
+//Godzilla attacks: 0-.33=rock .34-.67=paper .68-.99=scissors
+//If game is tied, Godzilla attack random 
+//If godzilla lost, select among other attacks
+//if godzilla won, no change in godzilla attack
+
+
+randomGodzillaattack = Math.random();
+console.log(randomGodzillaattack);
+
+godzillaLosesWithRock = Math.random();
+
+gozillaLosesWithPaper = Math.random();
+
+gozillaLosesWithScissors = Math.random();
+
+
 
 const selectedRadioButton = document.querySelector('input:checked');
 userAttack = selectedRadioButton.value;
